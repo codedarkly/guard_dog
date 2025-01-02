dda6d8df9599098822f9e07aaabcb665ec46f09c
Rocky Latchman <kidmongoose@gmail.com>
2025-01-01 20:12:41 -0800

    Password hashing and verification code methods added to the User class

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@ from mongoframes import *
 from datetime import datetime
 import random
 import string
-
+from passlib.hash import pbkdf2_sha256
 
 class User(Frame):
     _fields = {
@@ -23,20 +23,32 @@ class User(Frame):
     def check_user_account():
         pass
 
-    def hash_password():
-        pass
+    def hash_password(self, password):
+        return pbkdf2_sha256.hash(password)
 
-    def verify_password():
-        pass
+    def verify_password(self,password,hashed_password):
+        #get user from database and compare it against their enter password(use check_user_account())
+        return pbkdf2_sha256.verify(password, hashed_password)
 
+    @staticmethod
     def generate_verification_code():
-        pass
+        return generate_password(8)
 
-    def store_verification_code():
-        pass
+    def store_verification_code(self, rc, user):
+        user['verification_code'] = generate_verification_code()
+        user_data = f'username:{user["username"]}'
+        rc.hset(user_data, mapping=user)
+        rc.expire(user_data, 900)
+        result = rc.hgetall(user_data)
+        return {k.decode():v.decode() for k,v in result.items()}, 200
 
-    def verify_verification_code():
-        pass
+    def verify_verification_code(self, rc, **user):
+        try:
+            result = rc.hgetall(user['username'])
+            data = {k.decode():v.decode() for k,v in result.items()}
+            return data['verification_code']
+        except KeyError:
+            return 'Verification code or username is incorrect or your passcode has expired', 401
 
     def email_verification_code():
         pass
